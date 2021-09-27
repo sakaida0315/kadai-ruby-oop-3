54cfad2b8e9f30c931e7137f04921323d6119438
hiroki.sakaida <aronafirio1@yahoo.co.jp>
2021-09-27 13:02:08 +0000

    kadai-ruby-oop-3

--- human.rb
+++ human.rb
@@ -5,16 +5,13 @@ class Human<Animal
     
     attr_accessor :hobby
     
-    def initialize(hobby)
-    
-    self.hobby=hobby
+    def initialize(name,age,hobby)
+      self.name = name
+      self.age=age
+      self.hobby=hobby
     
      
     end
 end
-
-
-
-
 
 
