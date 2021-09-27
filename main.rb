43f6719713158b592c7b6db1da2c17bc90824a2c
hiroki.sakaida <aronafirio1@yahoo.co.jp>
2021-09-27 11:13:36 +0000

    kadai-ruby-oop-3

---
+++ main.rb
@@ -0,0 +1,28 @@
+
+require './human'
+
+
+    
+
+
+# インスタンスの作成
+
+animal=Animal.new("田中太郎",25)
+animal.say
+tanaka = Human.new("電車")
+tanaka.think
+animal=Animal.new("鈴木次郎",30)
+animal.say
+suzuki = Human.new("野球")
+suzuki.think
+animal=Animal.new("佐藤花子",20)
+animal.say
+sato   = Human.new("映画")
+sato.think
+
+
+
+
+
+
+
